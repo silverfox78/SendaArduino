b502731236a4abe20bbc52105cf4a2612ca8f5f1
Samuel Barrera <sbarrera@gmail.com>
2023-07-04 00:54:29 -0400

    Libreria - union

--- libreria/TemporamaLib/examples/Temporama_union/Temporama_union.ino
+++ libreria/TemporamaLib/examples/Temporama_union/Temporama_union.ino
@@ -1,30 +1,40 @@
 #include <Wire.h>
+#include <DS3231_Class.h>
+#include <MAX7219_Class.h>
 #include <SSD1306_CLass.h>
 
 #define BAUD_RATE 9600
 #define DELAYTIME 1000
 
+DS3231_Class clock;
+MAX7219_Class matriz;
 SSD1306_CLass led;
 
 void setup() {
   Wire.begin();
-  Serial.begin(BAUD_RATE);
+  Serial.begin(BAUD_RATE);  
+  matriz.initialize(15);
   led.initialize();
 }
 
 void loop() {
-  String text = "Ponyo";
+  Serial.print(String(clock.getTime()));
+  Serial.print(" - ");
+  Serial.print(clock.getSecond());
+  Serial.print(" - ");
+  Serial.print(clock.getSecondPercentage());
+  Serial.println("%");
+
+  String text = String(clock.getTime());
   int len = text.length();
+
   for (int i = 0; i < len; i++) {
     led.clear();
-    int16_t width, height, height_last;
-    led.measureText(width, height, 1, text.c_str());
-    led.printText(1, 64 - (width / 2), 8, text.c_str());
-    height_last = height;
+    int16_t width, height;
 
-    led.measureText(width, height, 2, text.c_str());
-    led.printText(2, 64 - (width / 2), height_last + 6, text.c_str());
-    height_last = height;
+    matriz.printText(text.substring(0, i + 1).c_str());
+    led.measureText(width, height, 3, text.substring(0, i + 1).c_str());
+    led.printText(3, 64 - (width / 2), 32 - (height/2), text.substring(0, i + 1).c_str());
 
     led.drawLine(3, 1, 120, 1);
     led.drawLine(1, 3, 122, 3);
@@ -42,11 +52,9 @@ void loop() {
     led.fillTriangle(111, 6, 117, 6, 117, 12);
     led.fillTriangle(6, 51, 12, 57, 6, 57);
     led.fillTriangle(117, 51, 117, 57, 111, 57);
-
-    led.measureText(width, height, 3, text.substring(0, i + 1).c_str());
-    led.printText(3, 64 - (width / 2), height_last + 15, text.substring(0, i + 1).c_str());
-
     led.display();
-    delay(DELAYTIME);
+    delay(DELAYTIME / 5);
   }
+
+  delay(DELAYTIME);
 }
